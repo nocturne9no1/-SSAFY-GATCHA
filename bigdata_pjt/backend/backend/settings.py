0cabb56e9809a8208e3473af83ad194ce1952254
Nocturne9no1 <nocturne9no1@gmail.com>
2021-11-15 09:48:14 +0900

    Del: delete private information

--- bigdata_pjt/backend/backend/settings.py
+++ bigdata_pjt/backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mk(#&59-1kn=*_hi##1cvznh=l*wj+pc_f3x+1o&4d+sba$+o8'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -102,13 +102,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ssafy_web_db',
-        'USER': 'root',
-        'PASSWORD': 'j5a402.p.ssafy',
-        'HOST': 'j5a402.p.ssafy.io',
-        'PORT': '3306'
+
     }
 }
 
